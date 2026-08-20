9d0a779984676e649236e168ebd75e9cdb9f51ae
Derek <derex1987@gmail.com>
2026-08-19 22:16:35 -0500

    initial

---
+++ src/client/java/io/github/derexxd/nosensesmod/client/NosensesmodClient.java
@@ -0,0 +1,10 @@
+package io.github.derexxd.nosensesmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NosensesmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/io/github/derexxd/nosensesmod/Nosensesmod.java
@@ -0,0 +1,10 @@
+package io.github.derexxd.nosensesmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nosensesmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}

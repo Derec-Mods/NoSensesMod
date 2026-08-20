435765871ad8e18b3a0146536926e5e2c62ff2fc
Derek <derex1987@gmail.com>
2026-08-19 22:59:28 -0500

    build: add java here so i dont have to change my home one

--- src/main/java/io/github/derexxd/nosensesmod/Nosensesmod.java
+++ src/main/java/io/github/derexxd/nosensesmod/Nosensesmod.java
@@ -1,10 +1,15 @@
 package io.github.derexxd.nosensesmod;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Nosensesmod implements ModInitializer {
+    public static final String MOD_ID = "nosensesmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        LOGGER.info("NoSensesMod loaded");
     }
 }

55929af50a75a5a60a430de66c0cacaf4906eb1f
Derek <derex1987@gmail.com>
2026-08-24 17:45:28 -0500

    style: fine tuning the headphones

--- src/client/java/io/github/derexxd/nosensesmod/client/model/HeadphoneModel.java
+++ src/client/java/io/github/derexxd/nosensesmod/client/model/HeadphoneModel.java
@@ -28,11 +28,11 @@ public class HeadphoneModel extends EntityModel<PlayerEntityRenderState> {
                 .uv(18, 5).cuboid(-7.5F, -2.0F, -2.0F, 1.0F, 4.0F, 4.0F, new Dilation(0.0F))
                 .uv(18, 13).cuboid(6.5F, -2.0F, -2.0F, 1.0F, 4.0F, 4.0F, new Dilation(0.0F))
                 .uv(0, 0).cuboid(-7.5F, -3.0F, -2.0F, 15.0F, 1.0F, 4.0F, new Dilation(0.0F)),
-                ModelTransform.pivot(0.0F, 16.0F, 0.0F));
+                ModelTransform.pivot(0.0F, 24.0F, 0.0F));
         root.addChild("earcups", ModelPartBuilder.create()
                 .uv(0, 5).cuboid(6.5F, 2.0F, -3.0F, 3.0F, 6.0F, 6.0F, new Dilation(0.0F))
                 .uv(0, 17).cuboid(-9.5F, 2.0F, -3.0F, 3.0F, 6.0F, 6.0F, new Dilation(0.0F)),
-                ModelTransform.pivot(0.0F, 16.0F, 0.0F));
+                ModelTransform.pivot(0.0F, 24.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 64);
     }
 }
